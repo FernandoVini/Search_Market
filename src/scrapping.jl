57b94106d39cc4e1fcf0b65e0d35fd582f8aaf16
gabriel-ino <chavesmartinsg@gmail.com>
2021-12-27 11:50:43 -0300

    Getting data on SQLite as DataFrame in Julia

--- src/scrapping.jl
+++ src/scrapping.jl
@@ -1,9 +1,35 @@
 using Pkg
 
-Pkg.add("SQLite")
+# Pkg.add("SQLite")
+# Pkg.add("DataFrames")
+# Pkg.add("InlineStrings")
+# Pkg.add("IterableTables")
+# Pkg.add(url="https://github.com/lungben/TableIO.jl")
+# Pkg.add("JDF")
+
+# Uncomment to install packages
 
 using SQLite
+using DataFrames
+using IterableTables
+using TableIO
 
-db = SQLite.DB("supermarkets.db")
 
-print(SQLite.tables(db))
+function getting_sites()
+    db = SQLite.DB("supermarkets.db")  # Reading database
+
+    println(SQLite.tables(db))
+    
+    df_sites = DataFrame(read_table(db, "supermarket"); copycols=false) # Converting SQLite database to DataFrame object
+
+    println(df_sites)
+
+    return df_sites
+
+
+end
+
+
+getting_sites()
+
+
